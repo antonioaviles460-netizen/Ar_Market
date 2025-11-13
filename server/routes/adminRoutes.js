61fc4d0292a161dab554ee06696e9790fbe713de
antonioaviles460-netizen <antonioaviles460@gmail.com>
2025-11-13 13:09:00 -0600

    Delete server directory

--- server/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, isAdmin } = require('../middleware/authMiddleware');
-const { getSalesHistory, updateProduct } = require('../controllers/adminController');
-
-router.get('/sales', protect, isAdmin, getSalesHistory);
-router.put('/products/:id', protect, isAdmin, updateProduct);
-
-module.exports = router;
